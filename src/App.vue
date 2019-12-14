2bcb7c9da151cbeefb01bfe4391691039995aedf
antoantonyk <antoantony8@gmail.com>
2019-12-14 16:21:10 +0530

    feat: add showcase and password strength component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,241 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form class="form-signup needs-validation" @submit="onSubmit" novalidate>
+      <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal">
+          Password Strength Meter - Demo
+        </h1>
+        <p>
+          To display the strength of the password with a visual feedback.
+          PasswordStrengthMeter use
+          <a href="https://github.com/dropbox/zxcvbn">zxcvbn</a> to estimate the
+          strength of the password and also provide a visual feedback with
+          suggestions and warning messages.
+        </p>
+      </div>
+
+      <div class="form-label-group">
+        <input
+          type="text"
+          id="inputFullname"
+          class="form-control"
+          placeholder="Fullname"
+          autocomplete="name"
+          v-model="form.fullname"
+          :class="{ 'is-invalid': error.fullname }"
+        />
+        <label for="inputFullname">Fullname</label>
+        <div class="invalid-feedback">Please provide a Fullname.</div>
+      </div>
+
+      <div class="form-label-group">
+        <input
+          type="text"
+          id="inputEmail"
+          class="form-control"
+          placeholder="Email address"
+          v-model="form.email"
+          autocomplete="email"
+          :class="{ 'is-invalid': error.email }"
+        />
+        <label for="inputEmail">Email address</label>
+        <div class="invalid-feedback">Please provide a email.</div>
+      </div>
+
+      <div class="form-label-group">
+        <input
+          type="password"
+          id="inputPassword"
+          class="form-control"
+          placeholder="Password"
+          aria-describedby="passwordHelpBlock"
+          v-model="form.password"
+          autocomplete="current-password"
+          :class="{ 'is-invalid': error.password }"
+        />
+        <label for="inputPassword">Password</label>
+        <PasswordStengthMeter
+          :password="form.password"
+          :enableFeedback="true"
+          @strengthChange="onPasswordStrengthChanged($event)"
+        ></PasswordStengthMeter>
+        <small id="passwordHelpBlock" class="form-text text-muted">
+          Your password must be 8-20 characters long, contain letters and
+          numbers, and must not contain spaces, special characters, or emoji.
+        </small>
+        <div class="invalid-feedback">Please provide a valid Password.</div>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
+      </div>
+
+      <button
+        id="btn-submit"
+        class="btn btn-lg btn-primary btn-block"
+        type="submit"
+      >
+        Sign up
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PasswordStengthMeter from "./components/PasswordStengthMeter.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    PasswordStengthMeter
+  },
+  data: function() {
+    return {
+      error: {
+        fullname: false,
+        email: false,
+        password: false
+      },
+      form: { fullname: "", email: "", password: "" }
+    };
+  },
+  methods: {
+    onPasswordStrengthChanged(strength) {
+      console.log("====================================");
+      console.log("onPasswordStrengthChanged", strength);
+      console.log("====================================");
+    },
+    onSubmit(e) {
+      e.preventDefault();
+      if (this.isFormValid()) {
+        console.log("====================================");
+        console.log("userForm", this.form);
+        console.log("====================================");
+      } else {
+        console.log("====================================");
+        console.log("userForm Validation Failed", this.form);
+        console.log("====================================");
+      }
+    },
+    isFormValid() {
+      const { fullname, email, password } = this.form;
+
+      if (fullname) {
+        this.error.fullname = false;
+      } else {
+        this.error.fullname = true;
+      }
+
+      if (email) {
+        this.error.email = false;
+      } else {
+        this.error.email = true;
+      }
+
+      if (password) {
+        this.error.password = false;
+      } else {
+        this.error.password = true;
+      }
+
+      return Object.keys(this.error).every(key => !this.error[key]);
+    }
   }
 };
 </script>
 
 <style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+:root {
+  --input-padding-x: 0.75rem;
+  --input-padding-y: 0.75rem;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+body {
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .form-signup {
+    width: 100%;
+    max-width: 420px;
+    padding: 15px;
+    margin: auto;
+  }
+
+  .form-control {
+    height: auto !important;
+  }
+
+  .form-label-group {
+    position: relative;
+    margin-bottom: 1rem;
+  }
+
+  .form-label-group > input,
+  .form-label-group > label {
+    padding: var(--input-padding-y) var(--input-padding-x);
+  }
+
+  .form-label-group > label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    margin-bottom: 0;
+    /* Override default `<label>` margin */
+    line-height: 1.5;
+    color: #495057;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+    transition: all 0.1s ease-in-out;
+  }
+
+  .form-label-group input::-webkit-input-placeholder {
+    color: transparent;
+  }
+
+  .form-label-group input:-ms-input-placeholder {
+    color: transparent;
+  }
+
+  .form-label-group input::-ms-input-placeholder {
+    color: transparent;
+  }
+
+  .form-label-group input::-moz-placeholder {
+    color: transparent;
+  }
+
+  .form-label-group input::placeholder {
+    color: transparent;
+  }
+
+  .form-label-group input:not(:placeholder-shown) {
+    padding-top: calc(
+      var(--input-padding-y) + var(--input-padding-y) * (2 / 3)
+    );
+    padding-bottom: calc(var(--input-padding-y) / 3);
+  }
+
+  .form-label-group input:not(:placeholder-shown) ~ label {
+    padding-top: calc(var(--input-padding-y) / 3);
+    padding-bottom: calc(var(--input-padding-y) / 3);
+    font-size: 12px;
+    color: #777;
+  }
 }
 </style>
---
+++ src/components/PasswordStengthMeter.vue
@@ -0,0 +1,187 @@
+<template>
+  <div>
+    <div class="strength-meter">
+      <div
+        class="strength-meter-fill"
+        :style="{ backgrond: meterColor }"
+        :data-strength="passwordStrength"
+      ></div>
+    </div>
+    <template v-if="enableFeedback && feedback">
+      <small class="password-feedback" v-if="feedback.warning">
+        {{ feedback.warning }}
+      </small>
+      <small
+        class="password-suggetion"
+        v-if="feedback.suggestions && feedback.suggestions.length > 0"
+      >
+        <template v-for="suggetion in feedback.suggestions">
+          {{ suggetion }}
+        </template>
+      </small>
+    </template>
+  </div>
+</template>
+
+<script>
+import * as zxcvbn from "zxcvbn";
+
+const defaultColours = [
+  "darkred",
+  "orangered",
+  "orange",
+  "yellowgreen",
+  "green"
+];
+
+export default {
+  name: "PasswordStengthMeter",
+  props: {
+    password: {
+      type: String,
+      required: true
+    },
+    minPasswordLength: {
+      type: Number,
+      default: 8
+    },
+    enableFeedback: Boolean,
+    colors: {
+      type: Array,
+      default: function() {
+        return defaultColours;
+      }
+    }
+  },
+  data() {
+    return {
+      passwordStrength: null,
+      feedback: {
+        suggestions: [],
+        warning: ""
+      },
+      prevPasswordStrength: null
+    };
+  },
+  watch: {
+    password(newPassword) {
+      if (!newPassword) {
+        this.passwordStrength = null;
+      } else if (newPassword && newPassword.length < this.minPasswordLength) {
+        this.passwordStrength = 0;
+      } else {
+        if (this.enableFeedback) {
+          const result = this.scoreWithFeedback(this.password);
+          this.passwordStrength = result.score;
+          this.feedback = result.feedback;
+        } else {
+          this.passwordStrength = this.score(this.password);
+          this.feedback = null;
+        }
+      }
+
+      // Only emit the passwordStrength if it changed
+      if (this.prevPasswordStrength !== this.passwordStrength) {
+        this.$emit("strengthChange", this.passwordStrength);
+        this.prevPasswordStrength = this.passwordStrength;
+      }
+    }
+  },
+  computed: {
+    meterColor() {
+      if (!this.strength || this.strength < 0 || this.strength > 5) {
+        return this.colors[0] ? this.colors[0] : this.defaultColours[0];
+      }
+
+      return this.colors[this.strength]
+        ? this.colors[this.strength]
+        : this.defaultColours[this.strength];
+    }
+  },
+  methods: {
+    score(password) {
+      const result = zxcvbn(password);
+      return result.score;
+    },
+    scoreWithFeedback(password) {
+      const result = zxcvbn(password);
+      return { score: result.score, feedback: result.feedback };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.strength-meter {
+  position: relative;
+  height: 3px;
+  background: #ddd;
+  margin: 10px auto 10px;
+  border-radius: 3px;
+}
+
+.strength-meter:before,
+.strength-meter:after {
+  content: "";
+  height: inherit;
+  background: transparent;
+  display: block;
+  border-color: #fff;
+  border-style: solid;
+  border-width: 0 5px 0 5px;
+  position: absolute;
+  width: calc(((100% - 20px) / 5) + 10px);
+  z-index: 10;
+}
+
+.strength-meter:before {
+  left: calc((100% - 20px) / 5);
+}
+
+.strength-meter:after {
+  right: calc((100% - 20px) / 5);
+}
+
+.strength-meter-fill {
+  background: transparent;
+  height: inherit;
+  position: absolute;
+  width: 0;
+  border-radius: inherit;
+  transition: width 0.5s ease-in-out, background 0.25s;
+}
+
+.strength-meter-fill[data-strength="0"] {
+  background: darkred;
+  width: 20%;
+}
+
+.strength-meter-fill[data-strength="1"] {
+  background: orangered;
+  width: 40%;
+}
+
+.strength-meter-fill[data-strength="2"] {
+  background: orange;
+  width: 60%;
+}
+
+.strength-meter-fill[data-strength="3"] {
+  background: yellowgreen;
+  width: 80%;
+}
+
+.strength-meter-fill[data-strength="4"] {
+  background: green;
+  width: 100%;
+}
+
+.password-feedback,
+.password-suggetion {
+  font-size: 70%;
+  font-weight: 400;
+  color: #6c757d !important;
+  display: inline-block;
+  margin-top: 0.25rem;
+}
+</style>
